a16b9345c1b6491514160e9628bd962c6ed1a428
Jorgitor19 <91979954+Jorgitor19@users.noreply.github.com>
2022-01-27 23:38:26 +0100

    Add files via upload

---
+++ cms2/view/admin/index.php
@@ -0,0 +1,7 @@
+<br>
+<br>
+
+<h6>El panel de administración es el núcleo de la página web, aquí vas a poder controlar todo, desde los usuarios, las noticias, etc.</h6>
+<br>
+<br>
+<img src="https://c.tenor.com/0u4Oyi32gQ0AAAAd/por-fin-soy-admin.gif" alt="Girl in a jacket">
